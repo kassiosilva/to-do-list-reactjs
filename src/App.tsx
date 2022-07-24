525daaa4966c4b6cd06b67452d0aa7f080f57daf
Kássio Silva <kassio.slva@gmail.com>
2022-07-24 10:50:56 -0300

    Criando component de lista de tarefas

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,28 @@
 import { PlusCircle } from 'phosphor-react';
 
 import { Header } from './components/Header';
+import { MyTasksList } from './components/MyTasksList';
 
 import styles from './App.module.css';
-
 import './global.css'
-import { MyTasksList } from './components/MyTasksList';
 
 function App() {
   return (
     <>
       <Header />
 
-      <form className={styles.form}>
-        <input type="text" placeholder="Adicione uma nova tarefa" />
+      <main className={styles.wrapper}>
+        <form className={styles.form}>
+          <input type="text" placeholder="Adicione uma nova tarefa" />
 
-        <button type="submit">
-          Criar
-          <PlusCircle size={20} />
-        </button>
-      </form>
+          <button type="submit">
+            Criar
+            <PlusCircle size={20} />
+          </button>
+        </form>
 
-      <MyTasksList />
+        <MyTasksList />
+      </main>
     </>
   )
 }
--- src/components/MyTasksList.tsx
+++ src/components/MyTasksList.tsx
@@ -1,7 +1,32 @@
+import ClipbordImg from '../assets/clipboard.svg';
+
 import styles from './MyTasksList.module.css';
 
 export function MyTasksList() {
   return (
-    <div className={styles.toDoList} />
+    <section className={styles.myTasksList}>
+      <header>
+        <div className={styles.createdTasks}>
+          <strong>Tarefas criadas</strong>
+          <span className={styles.counter}>0</span>
+        </div>
+
+        <div className={styles.completedTasks}>
+          <strong>Concluídas</strong>
+          <span className={styles.counter}>0</span>
+        </div>
+      </header>
+
+      <div className={styles.content}>
+        <div className={styles.emptyList}>
+          <img src={ClipbordImg} alt="Lista vazia" />
+
+          <div className={styles.emptyListInfo}>
+            <strong>Você ainda não tem tarefas cadastradas</strong>
+            <p>Crie tarefas e organize seus itens a fazer</p>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
